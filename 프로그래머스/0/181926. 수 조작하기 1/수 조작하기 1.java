8b5f05ccf303e0ce60a3aa944b3adcd5d40b2206
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-17 01:38:24 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.77 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, String control) {
+      for (char a : control.toCharArray()) {
+            switch(a) {
+                case 'w':
+                    n++;
+                    break;
+                case 's':
+                    n--;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
