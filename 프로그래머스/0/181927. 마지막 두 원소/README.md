973bb8cc68e96b9871141aafb4141415bb619f9d
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-17 01:16:53 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/README.md
+++ 프로그래머스/0/181927. 마지막 두 원소/README.md
@@ -1,10 +1,10 @@
 # [level 0] 마지막 두 원소 - 181927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181927?language=java) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.03 ms
+메모리: 76.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 17일 01:05:54
+2024년 07월 17일 01:16:51
 
 ### 문제 설명
 
--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] new_list = new int[num_list.length + 1];
-        for (int i = 0; i < num_list.length; i++) {
-            new_list[i] = num_list[i];
-        }
+        int[] new_list = Arrays.copyOf(num_list,num_list.length + 1);
         int lastIndex = num_list.length -1;
-        new_list[lastIndex + 1] = (num_list[lastIndex] > num_list[lastIndex - 1] ? num_list[lastIndex] - num_list[lastIndex - 1] : (2 * num_list[lastIndex]));
+        
+        new_list[lastIndex + 1] = 
+            (num_list[lastIndex] > num_list[lastIndex - 1] ? 
+             num_list[lastIndex] - num_list[lastIndex - 1] : 
+             (2 * num_list[lastIndex]));
 
         return new_list;
     }
