31cc9845c8e2344e2a1fac9606e414ac3e1b05ca
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-21 19:25:24 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.59 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916?language=java) 
 
 ### 성능 요약
 
-메모리: 80.1 MB, 시간: 8.85 ms
+메모리: 80.8 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 13:57:28
+2024년 07월 21일 19:25:21
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,45 +1,24 @@
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int answer = 0;
-        Map<Integer, Integer> cnt = IntStream.of(a, b, c, d)
-                .boxed()
-                .collect(Collectors.groupingBy(x -> x, Collectors.summingInt(e -> 1)));
+        int[] arr = {a, b, c, d};
+        Arrays.sort(arr);
 
-        List<Map.Entry<Integer, Integer>> sort = cnt.entrySet().stream() // 빈도수와 숫자 정렬
-                .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()))
-                .collect(Collectors.toList());
-
-        switch (sort.size()) {
-            case 1: // 4개가 같을 때
-                answer = 1111 * sort.get(0).getKey();
-                break;
-            case 4: // 4개가 모두 다를 때
-                answer = IntStream.of(a, b, c, d).min().orElse(0);
-                break;
-            case 2: 
-                int first = sort.get(0).getKey();
-                int second = sort.get(1).getKey();
-                int firstCnt = sort.get(0).getValue();
-                int secondCnt = sort.get(1).getValue();
-
-                if (firstCnt == 2 && secondCnt == 2) { // 두 개씩 같은 값
-                    answer = (first + second) * Math.abs(first - second);
-                } else if (firstCnt == 3 && secondCnt == 1){ //3개가 같고 1개가 다를 때
-                    answer = (int) Math.pow(10 * first + second, 2);
-                } else {
-                    answer = (int) Math.pow(10 * second + first, 2);
-                }
-                break;
-            case 3: // 3개, 1개일 때
-                int firstNum = sort.get(0).getKey();
-                int secondNum = sort.get(1).getKey();
-                int thirdNum = sort.get(2).getKey();
-                answer = (firstNum == 2) ? secondNum * thirdNum : secondNum * thirdNum;
-                break;
+        if (arr[0] == arr[3]) { // 4개가 같을 때
+            answer = 1111 * arr[0];
+        } else if (arr[0] == arr[2] || arr[1] == arr[3]) { // 3개만 같을 때
+            answer = (arr[0] == arr[2]) ? (int) Math.pow(10 * arr[0] + arr[3], 2)
+                    : (int) Math.pow(10 * arr[3] + arr[0], 2);
+        } else if (arr[0] == arr[1] && arr[2] == arr[3]) { // 2개씩 같을 때
+            answer = (arr[0] + arr[2]) * Math.abs(arr[0] - arr[2]);
+        } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[3]) { // 2개 같고, 1개, 1개
+            answer = (arr[0] == arr[1]) ? arr[2] * arr[3] : (arr[1] == arr[2]) ? 
+                    arr[0] * arr[3] : arr[0] * arr[1];
+        } else { // 모두 다를 때
+            answer = arr[0];
         }
+
         return answer;
     }
 }
