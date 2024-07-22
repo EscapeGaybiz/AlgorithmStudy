b9ab47a069037bf2a3eb9c24d62e2272e76f9517
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-22 23:19:45 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.07 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 부분 문자열 이어 붙여 문자열 만들기 - 181911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911?language=java) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.10 ms
+메모리: 76.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 22일 23:18:23
+2024년 07월 22일 23:19:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String[] my_strings, int[][] parts) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < my_strings.length; i++) {
-            int x = parts[i][0];
-            int y = parts[i][1];
-            answer.append(my_strings[i].substring(x, y + 1));
+            sb.append(my_strings[i].substring(parts[i][0], parts[i][1] + 1));
         }
-        return answer.toString();
+        return sb.toString();
     }
 }
