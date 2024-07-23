b362e0fd2a56507cd3d0d3f7926968a7967acf6b
gipark <gipark181@gmail.com>
2024-07-24 00:14:55 +0900

    Programmers/gipark/basic Day05 완료

---
+++ Programmers/gipark/basic/Day05/원소들의 곱과 합/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * 원소들의 곱과 합
+ * 제출 내역
+ * 문제 설명
+ * 정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * 2 ≤ num_list의 길이 ≤ 10
+ * 1 ≤ num_list의 원소 ≤ 9
+ * 입출력 예
+ * num_list	result
+ * [3, 4, 5, 2, 1]	1
+ * [5, 7, 8, 3]	0
+ * 입출력 예 설명
+ * 입출력 예 #1
+ *
+ * 모든 원소의 곱은 120, 합의 제곱은 225이므로 1을 return합니다.
+ * 입출력 예 #2
+ *
+ * 모든 원소의 곱은 840, 합의 제곱은 529이므로 0을 return합니다.
+ */
+class Solution {
+    public int solution(int[] num_list) {
+        int[] num = new int[]{1, 0};     // num[0]: 모든 원소의 곱, num:[1]: 모든 원소의 합의 제곱
+
+        for (int i : num_list) {
+            num[0] *= i;
+            num[1] += i;
+        }
+        return num[0] < Math.pow(num[1], 2) ? 1: 0;
+    }
+}
