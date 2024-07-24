0bed4b332d9072ae6685aa02eb85f6f5f1155509
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-24 21:12:32 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.62 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/README.md
+++ 프로그래머스/0/181902. 문자 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 개수 세기 - 181902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902?language=java) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.12 ms
+메모리: 84.9 MB, 시간: 0.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 21:03:08
+2024년 07월 24일 21:12:30
 
 ### 문제 설명
 
--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,15 +1,8 @@
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        for (int j = 0; j < my_string.length(); j++) {
-            int a;
-            char ch = my_string.charAt(j);
-            if (ch >= 'A' && ch <= 'Z') {
-                a = ch - 'A';
-            } else {
-                a = ch - 'a' + 26;
-            }
-            answer[a] += 1;
+        for (char ch : my_string.toCharArray()) {
+            answer[Character.isUpperCase(ch) ? ch - 65 : ch - 71]++;
         }
         return answer;
     }
