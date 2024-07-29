72841249e4978134142fbc2ff1ac0c55d8c4a0c6
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-29 23:57:56 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.75 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 1 - 181883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883?language=java) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 7.06 ms
+메모리: 78 MB, 시간: 2.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 23:55:37
+2024년 07월 29일 23:57:54
 
 ### 문제 설명
 
--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-         for (int[] a : queries) {
-            int s = a[0];
-            int e = a[1];
-
-            for (int i = 0; i < arr.length; i++) {
-                if (i >= s && i <= e) {
-                    arr[i] = arr[i] + 1;
-                }
+        for (int[] a : queries) {
+            int s = a[0], e = a[1];
+            for (int i = s; i <= e; i++) {
+                arr[i]++;
             }
         }
         return arr;
