38a8d3f71bc2f2b98eedca7c7a853bc8c9ca630c
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-31 23:45:06 +0900

    [level 0] Title: x 사이의 개수, Time: 10.17 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,9 +1,8 @@
 import java.util.*;
 class Solution {
     public int[] solution(String myString) {
-        String b = myString.replaceAll("x", "-");
         List<Integer> list = new ArrayList<>();
-        for (String a : b.split("-", -1)) {
+        for (String a : myString.split("x", -1)) {
             list.add(a.length());
         }
         return list.stream().mapToInt(i->i).toArray();
