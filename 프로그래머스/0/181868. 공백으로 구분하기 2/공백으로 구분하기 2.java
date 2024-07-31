99bf1e74bf54a85c9fdea8df5256280af9787069
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-07-31 22:59:21 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.12 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,10 +1,6 @@
 import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
-        List<String> list = new ArrayList<>();
-        for (String a : my_string.split(" ")){
-            if(!Objects.equals(a, "")) list.add(a);
-        }
-        return list.toArray(new String[0]);
+        return my_string.trim().split("[ ]+");
     }
 }
