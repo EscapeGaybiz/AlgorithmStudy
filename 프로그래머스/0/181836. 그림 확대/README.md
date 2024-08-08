fe36c466ac6d07fb3125857598ccfd531e99eb9f
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-08-09 00:37:52 +0900

    [level 0] Title: 그림 확대, Time: 0.64 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/README.md
+++ 프로그래머스/0/181836. 그림 확대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 그림 확대 - 181836 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181836) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181836?language=java) 
 
 ### 성능 요약
 
-메모리: 84.3 MB, 시간: 0.69 ms
+메모리: 74.6 MB, 시간: 0.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 00:11:22
+2024년 08월 09일 00:37:50
 
 ### 문제 설명
 
--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,21 +1,17 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
-        String[] a = new String[picture.length * k];
-        int idx = 0;
-        
-        for (String s : picture) {
+        String[] box = new String[picture.length * k];
+        for (int i = 0; i < picture.length; i++) {
             StringBuilder sb = new StringBuilder();
-            //문자 k번 반복 -> 가로 확대
-            for (char ch : s.toCharArray()) {
+            //문자 k번 반복 -> 가로
+            for (char ch : picture[i].toCharArray()) {
                 sb.append(String.valueOf(ch).repeat(k));
             }
-            //한줄 k번 반복 -> 세로 확대
-            String b = sb.toString();
-           
-            for (int i = 0; i < k; i++) {
-                a[idx++] = b;
+            //한줄 k번 반복 -> 세로
+            for (int j = 0; j < k; j++) {
+                box[i * k + j] = sb.toString();
             }
         }
-        return a;
+        return box;
     }
 }
