a15bcd29cd3ec9948f5740f54ef11dbb504cb3ed
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-06 11:11:45 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.87 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/README.md
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 삼각형의 완성조건 (1) - 120889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120889?language=java) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.07 ms
+메모리: 71.7 MB, 시간: 0.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 11:09:50
+2024년 09월 06일 11:11:42
 
 ### 문제 설명
 
--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,8 +1,7 @@
+import java.util.*;
 class Solution {
     public int solution(int[] sides) {
-        int b = Math.max(sides[0], Math.max(sides[1], sides[2]));
-        int c = Math.min(sides[0], Math.min(sides[1], sides[2]));
-        int a = sides[0] + sides[1] + sides[2] - b - c;
-        return b < a + c ? 1 : 2;
+        Arrays.sort(sides);
+        return sides[2] < sides[0] + sides[1] ? 1 : 2;
     }
 }
