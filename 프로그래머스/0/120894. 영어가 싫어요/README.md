14d52cadcf5d187f1a7ab483e82be4f2f0df1726
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-07 12:54:00 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.25 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/README.md
+++ 프로그래머스/0/120894. 영어가 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 영어가 싫어요 - 120894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=java) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.19 ms
+메모리: 75.3 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 12:53:17
+2024년 09월 07일 12:53:58
 
 ### 문제 설명
 
--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -5,10 +5,9 @@ class Solution {
                 "zero", "0", "one", "1", "two", "2", "three", "3", "four", "4",
                 "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"
         );
-        String result = numbers;
         for (String key : map.keySet()) {
-            result = result.replace(key, map.get(key));
+            numbers = numbers.replace(key, map.get(key));
         }
-        return Long.parseLong(result);
+        return Long.parseLong(numbers);
     }
 }
