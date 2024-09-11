07fbffcbbb2da40aa56b15ece41794c6c82e15f4
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-12 00:44:20 +0900

    [level 0] Title: 배열의 유사도, Time: 0.14 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/README.md
+++ 프로그래머스/0/120903. 배열의 유사도/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 유사도 - 120903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903?language=java) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 0.13 ms
+메모리: 82.6 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 12일 00:42:48
+2024년 09월 12일 00:44:18
 
 ### 문제 설명
 
--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,11 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(String[] s1, String[] s2) {
+        Set<String> set1 = new HashSet<>(Arrays.asList(s1));
         Set<String> set2 = new HashSet<>(Arrays.asList(s2));
-        int answer = 0;
-        for (String a : s1) {
-            if (set2.contains(a)) answer++;
-        }
-        return answer;
+        set1.retainAll(set2);
+        return set1.size();
     }
 }
