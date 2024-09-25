f15ceb9bcae35d49e26aed264bdee53c69dfd97d
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-25 17:13:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.50 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,16 +1,9 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        if (numbers.length > 2) {
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = 0; j < numbers.length; j++) {
-                    if(numbers[i] != numbers[j])
-                        answer = Math.max(numbers[i] * numbers[j], answer);
-                }
-            }
-        } else {
-            answer = numbers[0] * numbers[1];
-        }
-        return answer;
+        Arrays.sort(numbers);
+        int a = numbers[0] * numbers[1];
+        int b = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        return a > b ? a : b;
     }
 }
