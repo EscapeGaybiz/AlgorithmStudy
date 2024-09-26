1b8f78840464ac8343b46428730193f5e2f090b9
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-26 15:45:45 +0900

    [level 0] Title: 외계어 사전, Time: 1.91 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/README.md
+++ 프로그래머스/0/120869. 외계어 사전/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계어 사전 - 120869 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869?language=java) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 0.07 ms
+메모리: 80.4 MB, 시간: 1.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 15:42:58
+2024년 09월 26일 15:45:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,14 +1,10 @@
+import java.util.*;
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        for (int i = 0; i < dic.length; i++) {
-            boolean match = true;
-            for (int j = 0; j < spell.length; j++) {
-                if (!dic[i].contains(spell[j])) {
-                    match = false;
-                    break;
-                }
+        for (String word : dic) {
+            if (Arrays.stream(spell).allMatch(word::contains)) {
+                return 1;
             }
-            if (match) return 1;
         }
         return 2;
     }
