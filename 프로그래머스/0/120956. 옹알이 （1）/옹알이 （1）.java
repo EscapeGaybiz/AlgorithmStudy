826dd8a8186f031ef3320eb7ab688144a8f40dee
jeoniee <88180931+Jeoniee@users.noreply.github.com>
2024-09-27 16:39:09 +0900

    [level 0] Title: 옹알이 (1), Time: 8.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,10 +2,7 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for (String b : babbling) {
-            b = b.replaceAll("aya", " ")
-                 .replaceAll("ye", " ")
-                 .replaceAll("woo", " ")
-                 .replaceAll("ma", " ");
+            b = b.replaceAll("aya|ye|woo|ma", " ");
             if (b.matches("^[^a-zA-Z]*$")) answer++;
         }
         return answer;
